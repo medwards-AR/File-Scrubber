3d1269aa2604e891ca755a6850fd41254a2d8ddb
medwards-AR <62022669+medwards-AR@users.noreply.github.com>
2020-03-10 14:14:02 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		String dirName = "C:\\Users\\me023229\\Documents\\Lab_-_File_Scrubber.zip\\Lab - File Scrubber\\root";
+		
+	}
+		
+}
